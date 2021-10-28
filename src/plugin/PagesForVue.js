46c77a71c930a528c3dcaed7e845e7fe743985db
Koen Schouten <schouten_koen@hotmail.com>
2021-10-28 13:40:48 +0200

    Added a method for elidedPageRange. Added checks to see if  pageCount > 0.

--- src/plugin/PagesForVue.js
+++ src/plugin/PagesForVue.js
@@ -1,29 +1,51 @@
 import Paginator from "./Paginator.js"
 
 export default {
-    install(Vue, options){
-
+    install(Vue, options) {
         Vue.mixin({
             data() {
                 return {
-                    paginator: "",
+                    elidedPageRangeProps: {
+                        symbol: "...",
+                        onEachSide: 3,
+                        onEnds: 2,
+                    },
+                    paginator: new Paginator([], 1),
                     currentPageNumber: 1
                 }
             },
             methods: {
-                setPaginator : function(objectList, objectsPerPage){
+                setPaginator: function (objectList, objectsPerPage) {
                     this.paginator = new Paginator(objectList, objectsPerPage);
                 },
-                gotoPage : function(pageNumber){
+                gotoPage: function (pageNumber) {
                     this.currentPageNumber = pageNumber;
                 }
             },
             computed: {
-                page(){
-                    return this.paginator.getPage(this.currentPageNumber).getObjectList();
+                page() {
+                    if (this.paginator.pageCount() > 0) {
+                        return this.paginator.getPage(this.currentPageNumber);
+                    }
+                    return [];
+                }
+                ,
+                elidedPageRange() {
+                    if (this.paginator.pageCount() > 0) {
+                        return this.paginator.getElidedPageRange(this.currentPageNumber,
+                            this.elidedPageRangeProps.symbol,
+                            this.elidedPageRangeProps.onEachSide,
+                            this.elidedPageRangeProps.onEnds
+                        );
+                    } else {
+                        return [];
+                    }
+
                 }
             }
-
         });
     }
 }
+
+
+
